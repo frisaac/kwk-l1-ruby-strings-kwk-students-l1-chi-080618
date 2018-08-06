10219ae4f5bca1dae2f85476f1b7f46a36da15a0
frisaac <frisaac@stu.naperville203.org>
2018-08-06 21:22:41 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,11 @@
 # Code your prompts here!
-
+guest_name = gets.chomp
+party_name = gets.chomp
+date = gets.chomp
+time = gets.chomp
+host_name = gets.chomp
+puts "Dear #{guest_name},"
+puts "You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than October 30."
+puts "Sincerely"
+puts "#{host_name}"
 # Try starting out with puts'ing a string.
